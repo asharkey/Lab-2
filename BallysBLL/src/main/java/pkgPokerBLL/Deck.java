ef376f0cc341e4dd3f4035a215d738e27db3cc3c
Aidan <Aidan@128.4.90.50>
2017-03-07 15:50:13 -0500

    lab 2 implementing contrusctor

--- BallysBLL/src/main/java/pkgPokerBLL/Deck.java
+++ BallysBLL/src/main/java/pkgPokerBLL/Deck.java
@@ -4,26 +4,32 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 import pkgPokerEnum.eRank;
+import pkgPokerEnum.eSuit;
 
 public class Deck {
 
 	private UUID DeckID;
 	private ArrayList<Card> DeckCards = new ArrayList<Card>();
-	
-	public Deck()
-	{
-		//TODO: Implement This Constructor (no-arg Deck should build up a deck with 52 cards)
-		
-		//	This method will do a for/each, returning each rank in the enum.
+
+	public Deck() {
+		// TODO: Implement This Constructor (no-arg Deck should build up a deck
+		// with 52 cards)
+
+		// This method will do a for/each, returning each rank in the enum.
 		for (eRank Rank : eRank.values()) {
-			System.out.println(Rank.getiRankNbr());
+			for (eSuit Suit : eSuit.values()) {
+				if (Rank != eRank.JOKER && Suit != eSuit.JOKER) {
+					Card c = new Card(Rank, Suit);
+					DeckCards.add(c);
+					//System.out.println(Rank + " " + Suit);
+				}
+			}
 		}
 	}
-	
-	public Card DrawCard()
-	{
-		//	TODO: Implement this method... should draw a card from the deck.
-		
+
+	public Card DrawCard() {
+		// TODO: Implement this method... should draw a card from the deck.
+
 		return null;
 	}
 }
